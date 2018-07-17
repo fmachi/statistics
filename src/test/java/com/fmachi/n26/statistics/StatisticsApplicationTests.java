2e95e9712337b9a3a0fc3ab55533d96af0ef28de
Filippo Machi <filippo.machi@weekendesk.fr>
2018-07-17 23:53:33 +0200

    Added test about purging transactions

--- src/test/java/com/fmachi/n26/statistics/StatisticsApplicationTests.java
+++ src/test/java/com/fmachi/n26/statistics/StatisticsApplicationTests.java
@@ -1,11 +1,14 @@
 package com.fmachi.n26.statistics;
 
 import com.fmachi.n26.statistics.domain.Transaction;
+import com.fmachi.n26.statistics.domain.TransactionRepository;
 import com.fmachi.n26.statistics.dtos.StatisticsDTO;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -28,9 +31,17 @@ public class StatisticsApplicationTests {
     @LocalServerPort
     private int localServerPort;
 
+    @Autowired
+    @Qualifier("TransactionRepository")
+    TransactionRepository transactionRepository;
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @BeforeEach
+    void setup() {
+        transactionRepository.clear();
+    }
 
     @Test
     void shouldRejectExpiredTransaction() {
@@ -45,22 +56,19 @@ public class StatisticsApplicationTests {
 
     @Test
     void shouldCalculateStatistics() {
-        IntStream.rangeClosed(1,25).forEach(
+        IntStream.rangeClosed(1, 25).forEach(
                 i -> {
                     ResponseEntity<Void> response = restTemplate.postForEntity(
                             getTransactionUrl(),
-                            new Transaction(now() - seconds(30-i), new BigDecimal(i+"4.25")),
+                            new Transaction(now() - seconds(30 - i), new BigDecimal(i + "4.25")),
                             Void.class);
 
                     checkStatus(HttpStatus.CREATED, response.getStatusCode());
                 }
         );
 
-        ResponseEntity<StatisticsDTO> response = restTemplate.getForEntity(getStatisticsUrl(), StatisticsDTO.class);
+        StatisticsDTO statistics = getStatistics();
 
-        checkStatus(HttpStatus.OK, response.getStatusCode());
-
-        StatisticsDTO statistics = response.getBody();
         Assertions.assertEquals(new BigDecimal("134.25"), statistics.getAvg());
         Assertions.assertEquals(new BigDecimal("14.25"), statistics.getMin());
         Assertions.assertEquals(new BigDecimal("254.25"), statistics.getMax());
@@ -69,12 +77,56 @@ public class StatisticsApplicationTests {
     }
 
     @Test
+    void shouldPurgeStatistics() {
+        IntStream.rangeClosed(1, 15).forEach(
+                i -> {
+                    ResponseEntity<Void> response = restTemplate.postForEntity(
+                            getTransactionUrl(),
+                            new Transaction(now() - seconds(59), new BigDecimal(i + "4.25")),
+                            Void.class);
+
+                    checkStatus(HttpStatus.CREATED, response.getStatusCode());
+                }
+        );
+
+        IntStream.rangeClosed(16, 25).forEach(
+                i -> {
+                    ResponseEntity<Void> response = restTemplate.postForEntity(
+                            getTransactionUrl(),
+                            new Transaction(now() - seconds(30 - i), new BigDecimal(i + "4.25")),
+                            Void.class);
+
+                    checkStatus(HttpStatus.CREATED, response.getStatusCode());
+                }
+        );
+
+        StatisticsDTO statistics = getStatistics();
+
+        Assertions.assertEquals(new BigDecimal("134.25"), statistics.getAvg());
+        Assertions.assertEquals(new BigDecimal("14.25"), statistics.getMin());
+        Assertions.assertEquals(new BigDecimal("254.25"), statistics.getMax());
+        Assertions.assertEquals(new BigDecimal("3356.25"), statistics.getSum());
+        Assertions.assertEquals(Integer.valueOf(25), statistics.getCount());
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
+
+        statistics = getStatistics();
+
+        Assertions.assertEquals(new BigDecimal("209.25"), statistics.getAvg());
+        Assertions.assertEquals(new BigDecimal("164.25"), statistics.getMin());
+        Assertions.assertEquals(new BigDecimal("254.25"), statistics.getMax());
+        Assertions.assertEquals(new BigDecimal("2092.50"), statistics.getSum());
+        Assertions.assertEquals(Integer.valueOf(10), statistics.getCount());
+    }
+
+    @Test
     void shouldRetrieveEmptyStatistics() {
-        ResponseEntity<StatisticsDTO> response = restTemplate.getForEntity(getStatisticsUrl(), StatisticsDTO.class);
+        StatisticsDTO statistics = getStatistics();
 
-        checkStatus(HttpStatus.OK, response.getStatusCode());
-
-        StatisticsDTO statistics = response.getBody();
         Assertions.assertEquals(ZERO, statistics.getAvg());
         Assertions.assertEquals(ZERO, statistics.getMin());
         Assertions.assertEquals(ZERO, statistics.getMax());
@@ -106,6 +158,13 @@ public class StatisticsApplicationTests {
 
     private String getBaseUrl() {
         return LOCALHOST_URL + ":" + localServerPort;
+    }
+
+    private StatisticsDTO getStatistics() {
+        ResponseEntity<StatisticsDTO> response = restTemplate.getForEntity(getStatisticsUrl(), StatisticsDTO.class);
+
+        checkStatus(HttpStatus.OK, response.getStatusCode());
+        return response.getBody();
     }
 
 }
